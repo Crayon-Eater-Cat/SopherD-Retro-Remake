4c58aa8f55da220e2d903952a3fa426cad52374e
MUKILTEO\1717479 <1717479@mukilteo.wednet.edu>
2024-09-20 09:49:22 -0700

    Trying to make cursor thingy and new elevated platforms

--- Retro Remake/Assets/Scripts/Aim.cs
+++ Retro Remake/Assets/Scripts/Aim.cs
@@ -8,4 +8,8 @@ public class Aim : MonoBehaviour
     {
         PewRotate();
     }
+    private void PewRotate()
+    {
+
+    }
 }
---
+++ Retro Remake/Assets/Scripts/Cursor.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+    private Texture2D cursor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
